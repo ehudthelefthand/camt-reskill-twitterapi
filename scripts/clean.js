3ae4e86e10d666863ed9f5d9b006b4a6de0832c0
Peerawat Poombua <ehudthelefthand@Peerawats-MacBook-Pro.local>
2021-12-07 21:20:23 +0700

    added clean script

---
+++ scripts/clean.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('../models/user')
+const Tweet = require('../models/tweet')
+
+async function run() {
+    const tweets = await Tweet.find().populate('author').exec()
+    const hangingTweets = tweets.filter(t => t.author === null)
+    console.log(hangingTweets)
+    const ids = hangingTweets.map(ht => ht._id)
+    await Tweet.deleteMany({ _id: { $in: ids } })
+}
+
+mongoose.connect('mongodb://localhost/twitter_db')
+    .then(() => run())
+    .catch(err => {
+        console.error(err)
+    })
